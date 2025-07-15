4cd28bdcac430edf1f626d65baf7b9abb8ab9025
dattm97 <dattm@payme.vn>
2025-07-15 21:14:42 +0700

    feat: update

---
+++ App.tsx
@@ -0,0 +1,49 @@
+/**
+ * Sample React Native App
+ * https://github.com/facebook/react-native
+ *
+ * @format
+ */
+
+import { StatusBar, StyleSheet, useColorScheme, View } from 'react-native';
+import { MotiImage, MotiView } from 'moti';
+
+function App() {
+  const isDarkMode = useColorScheme() === 'dark';
+
+  return (
+    <View style={styles.container}>
+      <StatusBar barStyle={isDarkMode ? 'light-content' : 'dark-content'} />
+
+      <MotiView
+        transition={{ type: 'timing', duration: 500, delay: 200 }}
+        from={{ scale: 0, opacity: 0.8 }}
+        animate={{ scale: 1, opacity: 1 }}
+      >
+        <MotiImage
+          transition={{ type: 'timing', duration: 400, loop: true, delay: 100 }}
+          animate={{
+            scale: [1, 1.02, 1, 0.98],
+            opacity: [1, 0.8],
+            transform: [
+              {
+                rotateZ: ['0deg', '10deg', '0deg', '10deg', '0deg', '-10deg'],
+              },
+            ],
+          }}
+          source={{ uri: 'https://picsum.photos/200/300' }}
+          resizeMode="contain"
+          style={{ height: 180, width: 180 }}
+        />
+      </MotiView>
+    </View>
+  );
+}
+
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+  },
+});
+
+export default App;
